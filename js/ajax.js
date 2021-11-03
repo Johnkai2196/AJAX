10e77e4aab02942acf38d43ffe4e47b2b8f768f2
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2021-11-03 22:58:19 +0200

    added js code and image if now picture avaible

--- js/ajax.js
+++ js/ajax.js
@@ -1,6 +1,6 @@
 'use strict';
-const fetchShow = async () => {
-  const response = await fetch('https://api.tvmaze.com/search/shows?q=squ');
+const fetchShow = async (url) => {
+  const response = await fetch(url);
   return await response.json();
 
 };
@@ -8,48 +8,96 @@ const fetchShow = async () => {
 const form = document.querySelector('#search-form');
 const target = document.querySelector('#target');
 const query = document.querySelector('#query');
-const main = document.querySelector('main');
+
 form.addEventListener('submit', async (ev) => {
   ev.preventDefault();
   const url = form.action + '?q=' + query.value;
   const tvShows = await fetchShow(url);
-
-  console.log(tvShows.length)
-  if (tvShows.length == 0) {
-
-    let main = document.querySelector('main');
-
+  console.log(tvShows);
+  if (document.contains(document.querySelector('img'))) {
+    document.querySelector('img').remove();
+  }
+  if (target.contains(target.querySelector('article'))) {
+    target.innerHTML = '';
+  }
+  if (tvShows.length === 0) {
     let img = document.createElement('img');
-    img.setAttribute('id', 'img');
-    img.src = 'cooltext363311245930538.png';
+    img.setAttribute('id', 'imgNo');
+    img.src = 'Welcome.png';
     img.alt = 'Welcome! What shows do you want to look up?';
-    main.appendChild(img);
+    target.appendChild(img);
   } else {
     for (let i = 0; i < tvShows.length; i++) {
-
-      const article = document.createElement('article');
-      const header = document.createElement('header');
-      const h2 = document.createElement('h2');
-      const figure = document.createElement('figure');
-      const img = document.createElement('img');
-      const p = document.createElement('p');
-      const figcaption = document.createElement('figcaption');
       console.log(tvShows[i].show.name)
-      h2.innerHTML = tvShows[i].show.name;
-      img.src = tvShows[i].show.image.medium;
-      img.alt = tvShows[i].show.title;
-      figcaption.innerHTML = tvShows[i].show.caption;
-      p.innerHTML = tvShows[i].show.description;
+      let article = document.createElement('article');
+      let header = document.createElement('header');
+      let h2 = document.createElement('h2');
+      let img = document.createElement('img');
+      let figure = document.createElement('figure');
+      let p = document.createElement('p');
+      let p2 = document.createElement('p');
+      let p3 = document.createElement('p');
+      let a = document.createElement('a');
 
-      header.appendChild(h2);
-      figure.appendChild(img);
-      figure.appendChild(figcaption);
+      let showName = '';
+      let genresText = '';
+      let summaryText = '';
 
-      article.appendChild(header);
-      article.appendChild(figure);
-      article.appendChild(p);
+      if (tvShows[i].show.name == null) {
+        showName = document.createTextNode('404');
+      } else {
+        showName = document.createTextNode(tvShows[i].show.name);
+      }
+      if (tvShows[i].show.image == null) {
+        img.src = 'noPicture.jpg';
+        img.alt = '404 no image';
+      } else {
+        img.src = tvShows[i].show.image.medium;
+        img.alt = tvShows[i].show.name;
+      }
+
+      if (tvShows[i].show.officialSite == null) {
+        let error = document.createTextNode('Sites not found');
+        p.appendChild(error);
+      } else {
+        a.setAttribute('href', tvShows[i].show.officialSite);
+        a.innerHTML = tvShows[i].show.name;
+      }
+
+      if (tvShows[i].show.genres == 0) {
+        genresText = document.createTextNode('Genres not found');
+      } else {
+        genresText = document.createTextNode(
+            'Genres: ' + tvShows[i].show.genres);
+      }
+
       target.appendChild(article);
+      article.appendChild(header);
+      header.appendChild(h2);
+      h2.appendChild(showName);
+      article.appendChild(figure);
+      figure.appendChild(img);
+      figure.appendChild(p);
+      figure.appendChild(a);
+      figure.appendChild(p2);
+      figure.appendChild(p3);
+      p2.appendChild(genresText);
+
+      if (tvShows[i].show.summary == 0) {
+        summaryText = document.createTextNode('Summary not found');
+        p3.appendChild(summaryText);
+      } else if (tvShows[i].show.summary == null) {
+        summaryText = document.createTextNode('Summary not found');
+        p3.appendChild(summaryText);
+      } else {
+        article.innerHTML += tvShows[i].show.summary;
+      }
     }
-    console.log(tvShows);
+  }
+});
+form.addEventListener('keyup', function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.querySelector('button[type="submit"]').click();
   }
 });
