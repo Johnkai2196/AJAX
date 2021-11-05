1cc22003b85ff523fd7e2fe4f59230393490920a
Johnkai2196 <johnkaic@metropolia.fi>
2021-11-05 16:01:35 +0200

    added to top

--- js/ajax.js
+++ js/ajax.js
@@ -8,7 +8,7 @@ const fetchShow = async (url) => {
 const form = document.querySelector('#search-form');
 const target = document.querySelector('#target');
 const query = document.querySelector('#query');
-const body= document.querySelector('body');
+const body = document.querySelector('body');
 
 form.addEventListener('submit', async (ev) => {
   ev.preventDefault();
@@ -29,7 +29,7 @@ form.addEventListener('submit', async (ev) => {
     body.appendChild(img);
   } else {
     for (let i = 0; i < tvShows.length; i++) {
-      console.log(tvShows[i].show.name)
+      console.log(tvShows[i].show.name);
       let article = document.createElement('article');
       let header = document.createElement('header');
       let h2 = document.createElement('h2');
@@ -99,3 +99,22 @@ form.addEventListener('submit', async (ev) => {
     }
   }
 });
+//Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
