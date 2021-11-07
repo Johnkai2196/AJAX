93b3c5bca706fc7d6040dc7dd59427e2b055b348
Johnkai2196 <johnkaic@metropolia.fi>
2021-11-07 12:22:50 +0200

    added a function that slides you back up

--- js/ajax.js
+++ js/ajax.js
@@ -100,16 +100,18 @@ form.addEventListener('submit', async (ev) => {
   }
 });
 //Get the button
-let mybutton = document.getElementById("myBtn");
+let mybutton = document.getElementById('myBtn');
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    mybutton.style.display = 'block';
   } else {
-    mybutton.style.display = "none";
+    mybutton.style.display = 'none';
   }
 }
 
